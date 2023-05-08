08c22a0bc84ea2b31738ed00d50611d18116b69f
Anupam Singh <79111711+AnupamAS01@users.noreply.github.com>
2023-05-08 09:56:30 +0530

    test

--- chainHawk.go
+++ chainHawk.go
@@ -1,35 +1,132 @@
 package main
 
 import (
-	"context"
+	"bufio"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
-
-	"github.com/google/go-github/github"
+	"strings"
 )
 
+type PackageJSON struct {
+	Dependencies map[string]string `json:"dependencies"`
+}
+
+type Repo struct {
+	Name string `json:"name"`
+}
+
 func main() {
-	fmt.Println("Enter the name of a GitHub organization:")
+	reader := bufio.NewReader(os.Stdin)
 
-	var orgName string
-	_, err := fmt.Scan(&orgName)
+	fmt.Print("Enter the GitHub organization name: ")
+	org, _ := reader.ReadString('\n')
+	org = strings.TrimSpace(org)
 
+	repos, err := fetchRepos(org)
 	if err != nil {
-		fmt.Println("Error reading input:", err)
+		fmt.Printf("Error fetching repositories: %v\n", err)
 		os.Exit(1)
 	}
 
-	client := github.NewClient(nil)
-	org, _, err := client.Organizations.Get(context.Background(), orgName)
+	for _, repo := range repos {
+		fmt.Printf("\nChecking repository: %s/%s\n", org, repo.Name)
 
+		packageJSON, err := fetchPackageJSON(org, repo.Name)
+		if err != nil {
+			fmt.Printf("Error fetching package.json: %v\n", err)
+			continue
+		}
+
+		if packageJSON == nil {
+			fmt.Println("package.json not found")
+		} else {
+			fmt.Println("package.json found")
+			checkDependencies(packageJSON)
+		}
+	}
+}
+
+func fetchRepos(org string) ([]Repo, error) {
+	url := fmt.Sprintf("https://api.github.com/orgs/%s/repos", org)
+
+	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Println("Error fetching organization data:", err)
-		os.Exit(1)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("error fetching repos, status code: %d", resp.StatusCode)
 	}
 
-	fmt.Printf("\nOrganization details for %s:\n", orgName)
-	fmt.Println("ID:", org.GetID())
-	fmt.Println("Name:", org.GetName())
-	fmt.Println("Description:", org.GetDescription())
-	fmt.Println("Public repos:", org.GetPublicRepos())
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var repos []Repo
+	err = json.Unmarshal(body, &repos)
+	if err != nil {
+		return nil, err
+	}
+
+	return repos, nil
+}
+
+func fetchPackageJSON(org, repoName string) (*PackageJSON, error) {
+	url := fmt.Sprintf("https://raw.githubusercontent.com/%s/%s/master/package.json", org, repoName)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, nil
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var packageJSON PackageJSON
+	err = json.Unmarshal(body, &packageJSON)
+	if err != nil {
+		return nil, err
+	}
+
+	return &packageJSON, nil
+}
+
+func checkDependencies(packageJSON *PackageJSON) {
+	for packageName := range packageJSON.Dependencies {
+		available, err := isPackageAvailable(packageName)
+		if err != nil {
+			fmt.Printf("Error checking package '%s': %v\n", packageName, err)
+			continue
+		}
+
+		if available {
+			fmt.Printf("Package '%s' is available\n", packageName)
+		} else {
+			fmt.Printf("Package '%s' is not available\n", packageName)
+		}
+	}
+}
+
+func isPackageAvailable(packageName string) (bool, error) {
+	url := fmt.Sprintf("https://registry.npmjs.org/%s", packageName)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
+
+	return resp.StatusCode == http.StatusOK, nil
 }
